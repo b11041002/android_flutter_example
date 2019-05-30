f94b319164d28a346c012a82c5692c908d439544
songjiabin <1796254117@qq.com>
2019-05-30 10:22:03 +0800

    flutter向android请求

--- MyFlutterApp/app/src/main/java/jeno/com/myflutterapp/MainActivity.java
+++ MyFlutterApp/app/src/main/java/jeno/com/myflutterapp/MainActivity.java
@@ -7,8 +7,15 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import io.flutter.facade.Flutter;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.view.FlutterView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static final String BATTERY_CHANNEL = "MyFlutterAPP.flutter.io/getBattery";
+    private View flutterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +24,37 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                View flutterView = Flutter.createView(MainActivity.this, getLifecycle(), "route2");
+                flutterView = Flutter.createView(MainActivity.this, getLifecycle(), "route2");
                 FrameLayout.LayoutParams layout = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
                 addContentView(flutterView, layout);
+                getBattery();
             }
         });
 
-
     }
+
+
+    public int getBattery() {
+        new MethodChannel((FlutterView) flutterView, BATTERY_CHANNEL).setMethodCallHandler(new MethodChannel.MethodCallHandler() {
+            @Override
+            public void onMethodCall(MethodCall methodCall, MethodChannel.Result result) {
+                //  methodCall 用来区分不同方法的调用
+                if (methodCall.method.equals("getBatteryLevel")) {
+                    //如果flutter调用的方法是：getBatteryLevel 那么执行
+                    //返回成功的回调方法
+                    if (true)
+                        result.success("100%电量");
+                    else {
+                        result.error("错误", "", null);
+                    }
+                } else {
+                    //其他方法的调用 表明没有对应实现。
+                    result.notImplemented();
+                }
+            }
+        });
+        return 0;
+    }
+
+
 }
--- my_flutter/lib/main.dart
+++ my_flutter/lib/main.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
+import 'package:flutter/services.dart';
 
 
-void main() => runApp(MyApp());
+void main() =>
+    runApp(new MaterialApp(
+      home: MyApp(),
+    ));
 
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: _getWidgetForRoute(window.defaultRouteName),
+    return new Scaffold(
+      body: _getWidgetForRoute(
+          window.defaultRouteName == null ? 'route1' : '电量'),
     );
   }
+
+
+  //和原生进行交互的时候 双方定义好的约定好的变量
+  static const MethodChannel _methodChannel = MethodChannel('MyFlutterAPP.flutter.io/getBattery');
+  String _batteryLebvel = '电量是 ：';
+
 
   //根据 Route得到相应的布局
   //当android调用的时候根据 route 得到相应的布局
@@ -23,10 +39,56 @@ class MyApp extends StatelessWidget {
         );
       case 'route2':
         return new Center(
-          child: Text('route22'),
+          child: Text('route223'),
         );
+      case '电量':
+        return _getBatteryLebvelWidget();
     }
   }
+
+  Widget _getBatteryLebvelWidget() {
+    return
+      Scaffold(
+          body: Center(
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(_batteryLebvel),
+                IconButton(
+                    icon: Icon(Icons.refresh, color: Colors.blue,),
+                    onPressed: () {
+                      //点击进行刷新
+                      _pressGetBattery();
+                    })
+              ],
+            ),
+          ));
+  }
+
+
+  //得到原生的电量
+  _pressGetBattery() async {
+    String battery;
+    //相当于调用原生中的方法 获取电量
+    try {
+      final String result = await _methodChannel.invokeMethod('getBatteryLevel');
+      print('点了'+result.toString());
+      battery = '电量是：$result';
+    } on PlatformException {
+      battery = '获取电量失败';
+    } catch (e) {
+      print(e);
+    }
+
+
+    setState(() {
+      _batteryLebvel = battery;
+    });
+  }
+
 }
+
+
+
 
 
